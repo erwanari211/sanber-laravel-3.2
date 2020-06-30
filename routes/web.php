fe37892f79076d2d2a8c18e16eddb4f64b207996
Erwan <erwan.ari211@gmail.com>
2020-06-30 12:35:28 +0700

    menambahkan home, register, dan welcome

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/register', 'AuthController@register')->name('register');
+Route::post('/welcome', 'AuthController@welcome')->name('welcome');
